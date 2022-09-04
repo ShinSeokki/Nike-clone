dc5ae7bbe63a3995b17eee6c846b9670e11e952b
baeian <ddoory1103@gmail.com>
2022-09-04 11:38:07 +0900

    ADD :: added footer-banner

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,8 +3,22 @@ import './Footer.css'
 
 function Footer(props) {
    return (
-      <div>
-         
+      <div className='footer-contents'>
+         <div className='footer-banner'>
+            고객님의 나이키 멤버 정보가 이전됩니다.
+            <a className='btn-open-modal'>동의하기</a>
+         </div>
+         <div className='footer-nav'>
+            <div className='uk-grid'>
+               
+            </div>
+         </div>
+         <div className='footer-copy'>
+            
+         </div>
+         <div className='footer-info'>
+
+         </div>
       </div>
    );
 }
